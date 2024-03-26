2b3841cd9fd90c538b089c237afb4c17369aef57
21-manic-004g <21-manic-004g@21-manic-004>
2024-03-26 15:43:51 +0900

    baek

---
+++ Back/src/Q2741/Main.java
@@ -0,0 +1,25 @@
+package Q2741;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int i = 0;
+		while(true) {
+			++i;
+			System.out.println(i);
+			
+			
+			if(a==i) {
+				sc.close();
+				break;
+			}
+			
+		}
+			
+	}
+
+}
